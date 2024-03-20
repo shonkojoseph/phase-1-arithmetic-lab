c67797a51c962edb5fefdd1ea4decec5acf39ff8
Joseph <josephshonko99@gmail.com>
2024-03-20 12:48:05 +0300

    commiting arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 20
+const num2 = 3.1
+let multiply = num1 * num2
+let random = Math . floor(Math.random() * 100) + 1;
+let max= Math.max(1,2,3,4,5,20)
+const num3 = 16
+const num4=4
+let mod=num3/num4
